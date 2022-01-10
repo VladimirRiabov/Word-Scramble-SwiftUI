f12ec06dda3894428af8d39dcf7ae894fb1eec45
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-10 23:32:23 +0300

    Initial Commit

---
+++ Word Scramble SwiftUI/Word_Scramble_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Scramble_SwiftUIApp.swift
+//  Word Scramble SwiftUI
+//
+//  Created by Владимир Рябов on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Word_Scramble_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
