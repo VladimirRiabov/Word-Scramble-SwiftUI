4e7be55bde6a5ca7568d9de0b3cb455412792b0f
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-11 18:30:27 +0300

    Validating words with UITextChecker – Word Scramble SwiftUI Tutorial 6/6

--- Word Scramble SwiftUI/ContentView.swift
+++ Word Scramble SwiftUI/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -34,6 +39,12 @@ struct ContentView: View {
                 addNewWord()
             }
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingAlert) {
+                Button("OK", role: .cancel) {
+                }
+            } message: {
+                Text(errorMessage)
+            }
             
         }
 
@@ -43,6 +54,22 @@ struct ContentView: View {
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else {return}
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You cant just make them up, you know")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
@@ -59,7 +86,45 @@ struct ContentView: View {
           fatalError("Could not load start.txt from bundle")
     }
     
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+//        let word = "swift"
+        let checker = UITextChecker()
+        //it is a way of storing letters inside of a string
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        //if there is no mistakes
+        return misspelledRange.location == NSNotFound
+        
+        
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingAlert = true
+    }
+    
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
