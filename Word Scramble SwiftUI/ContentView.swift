09e93ff5fa5c2149836cdb9bd9968e21b16ac02d
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-11 17:22:28 +0300

    Running code when our app launches – Word Scramble SwiftUI Tutorial 5/6

--- Word Scramble SwiftUI/ContentView.swift
+++ Word Scramble SwiftUI/ContentView.swift
@@ -33,6 +33,7 @@ struct ContentView: View {
             .onSubmit {
                 addNewWord()
             }
+            .onAppear(perform: startGame)
             
         }
 
@@ -46,8 +47,16 @@ struct ContentView: View {
             usedWords.insert(answer, at: 0)
         }
         
-        
-        
+    }
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL)  {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+          fatalError("Could not load start.txt from bundle")
     }
     
 }
